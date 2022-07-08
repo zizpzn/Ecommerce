1f3b573cd110428fe300f819f9f629fafa65505f
zizpzn <zizpzn@gmail.com>
2022-07-08 16:12:54 +0700

    controllers

---
+++ server/controllers/user.js
@@ -0,0 +1,7 @@
+exports.sayHi = (req, res) => {
+  res.json({ message: "Hello there" });
+};
+
+// export const sayHi = (req, res) => {
+//   res.json({ message: "Hello there" });
+// };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello from node haha");
-});
+const { sayHi } = require("../controllers/user");
+
+// router.get("/", (req, res) => {
+//   res.send("Hello from node haha");
+// });
+
+router.get("/", sayHi);
 
 module.exports = router;
