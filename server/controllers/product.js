e228174e8fa962556d2ffdf5b2ac8f30a9945ef2
zizpzn <zizpzn@gmail.com>
2022-07-13 14:57:20 +0700

    product by id middleware

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -3,6 +3,23 @@ const _ = require("lodash");
 const fs = require("fs");
 const Product = require("../models/product");
 
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: "Product not found",
+      });
+    }
+    req.product = product;
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
