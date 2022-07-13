94fd7796af963dbf2477137b99f8ebf071e16a67
zizpzn <zizpzn@gmail.com>
2022-07-13 10:50:45 +0700

    create category

--- server/app.js
+++ server/app.js
@@ -29,6 +29,7 @@ app.use(expressValidator());
 // routes middleware
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 const port = process.env.PORT || 8000;
 
