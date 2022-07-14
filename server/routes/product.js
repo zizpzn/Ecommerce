0b53c0d3b1f97791096139f55926741819987348
zizpzn <zizpzn@gmail.com>
2022-07-14 13:46:09 +0700

    related products

--- server/routes/product.js
+++ server/routes/product.js
@@ -8,6 +8,7 @@ const {
   remove,
   update,
   list,
+  listRelated,
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -30,6 +31,7 @@ router.put(
 );
 
 router.get("/products", list);
+router.get("/products/related/:productId", listRelated);
 
 router.param("userId", userById);
 router.param("productId", productById);
