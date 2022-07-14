097bb0ab29ba989164b75578f708fa7c812ba6c4
zizpzn <zizpzn@gmail.com>
2022-07-14 16:13:35 +0700

    Menu, active links

---
+++ front/src/core/Menu.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    console.log(history.location);
+    return { color: "#ff9900" };
+  } else {
+    console.log(history.location);
+    return { color: "#ffffff" };
+  }
+};
+
+const Menu = ({ history }) => (
+  <div>
+    <ul className="nav nav-tabs bg-primary">
+      <li className="nav-item">
+        <Link className="nav-link" style={isActive(history, "/")} to="/">
+          Home
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          style={isActive(history, "/signin")}
+          to="/signin"
+        >
+          Signin
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          style={isActive(history, "/signup")}
+          to="/signup"
+        >
+          Signup
+        </Link>
+      </li>
+    </ul>
+  </div>
+);
+
+export default withRouter(Menu);
