9bac4fb946cdc96a2664488f93718e8668588eda
zizpzn <zizpzn@gmail.com>
2022-07-14 10:44:26 +0700

    product by sell

--- server/routes/product.js
+++ server/routes/product.js
@@ -7,6 +7,7 @@ const {
   read,
   remove,
   update,
+  list,
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
@@ -27,6 +28,8 @@ router.put(
   isAdmin,
   update
 );
+
+router.get("/product", list);
 
 router.param("userId", userById);
 router.param("productId", productById);
