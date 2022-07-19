ed58f4144bb78f025e2f3f01d88df547b591145c
zizpzn <zizpzn@gmail.com>
2022-07-19 10:00:47 +0700

    handle categories toggle

--- front/src/core/Checkbox.js
+++ front/src/core/Checkbox.js
@@ -3,29 +3,29 @@ import React, { useState, useEffect } from "react";
 const Checkbox = ({ categories }) => {
   const [checked, setCheked] = useState([]);
 
-  // const handleToggle = (c) => () => {
-  //   // return the first index or -1
-  //   const currentCategoryId = checked.indexOf(c);
-  //   const newCheckedCategoryId = [...checked];
+  const handleToggle = (category) => () => {
+    // return the first index or -1
+    const currentCategoryId = checked.indexOf(category);
+    const newCheckedCategoryId = [...checked];
 
-  //   // if currently checked was not already in checked state > push
-  //   // else pull/take off
-  //   if (currentCategoryId === -1) {
-  //     newCheckedCategoryId.push(c);
-  //   } else {
-  //     newCheckedCategoryId.splice(currentCategoryId, 1);
-  //   }
+    // if currently checked was not already in checked state > push
+    // else pull / take off
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(category);
+    } else {
+      newCheckedCategoryId.splice(currentCategoryId, 1);
+    }
 
-  //   // console.log(newCheckedCategoryId);
-  //   setCheked(newCheckedCategoryId);
-  //   handleFilters(newCheckedCategoryId);
-  // };
+    console.log(newCheckedCategoryId);
+    setCheked(newCheckedCategoryId);
+    // handleFilters(newCheckedCategoryId);
+  };
 
   return categories.map((category, index) => (
     <li key={index} className="list-unstyled">
       <input
-        // onChange={handleToggle(category._id)}
-        // value={checked.indexOf(category._id === -1)}
+        onChange={handleToggle(category._id)}
+        value={checked.indexOf(category._id === -1)}
         type="checkbox"
         className="form-check-input"
       />
