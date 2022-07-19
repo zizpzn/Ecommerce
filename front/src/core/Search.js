bf64f4cd283f16acc4f49513d847bb18f2ee8934
zizpzn <zizpzn@gmail.com>
2022-07-19 14:35:29 +0700

    search form

--- front/src/core/Search.js
+++ front/src/core/Search.js
@@ -27,9 +27,48 @@ const Search = () => {
     loadCategories();
   }, []);
 
+  const searchSubmit = (event) => {
+    event.preventDefault();
+    // searchData();
+  };
+
+  const handleChange = (name) => (event) => {
+    setData({ ...data, [name]: event.target.value, searched: false });
+  };
+
+  const searchForm = () => (
+    <form onSubmit={searchSubmit}>
+      <span className="input-group-text">
+        <div className="input-group input-group-lg">
+          <div className="input-group-prepend">
+            <select className="btn mr-2" onChange={handleChange("category")}>
+              <option value="All">All</option>
+              {categories.map((category, index) => (
+                <option key={index} value={category._id}>
+                  {category.name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <input
+            type="search"
+            className="form-control"
+            onChange={handleChange("search")}
+            placeholder="Search by name"
+          />
+        </div>
+
+        <div className="btn input-group-append" style={{ border: "none" }}>
+          <button className="input-group-text">Search</button>
+        </div>
+      </span>
+    </form>
+  );
+
   return (
     <div className="row">
-      <div className="container mb-3">...</div>
+      <div className="container mb-3">{searchForm()}</div>
       <div className="container-fluid mb-3">...</div>
     </div>
   );
