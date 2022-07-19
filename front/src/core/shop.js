bbb4241af472c0cdb53db70d4550406bf6fe910f
zizpzn <zizpzn@gmail.com>
2022-07-19 10:50:19 +0700

    fixed price range

--- front/src/core/shop.js
+++ front/src/core/shop.js
@@ -3,6 +3,7 @@ import Layout from "./Layout";
 import Card from "./card";
 import { getCategories } from "./apiCore";
 import Checkbox from "./Checkbox";
+import { prices } from "./fixedPrices";
 
 const Shop = () => {
   const [myFilters, setMyFilters] = useState({
