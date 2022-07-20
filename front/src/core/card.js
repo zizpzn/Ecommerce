f403d4b9f703198e4200a64df87e196ccf9a3862
zizpzn <zizpzn@gmail.com>
2022-07-20 09:23:16 +0700

    product detail

--- front/src/core/card.js
+++ front/src/core/card.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import ShowImage from "./ShowImage";
+import moment from "moment";
 
 const Card = ({ product, showViewProductButton = true }) => {
   const showViewButton = (showViewProductButton) => {
@@ -15,17 +16,35 @@ const Card = ({ product, showViewProductButton = true }) => {
     );
   };
 
+  const showAddToCartButton = () => {
+    return (
+      <button className="btn btn-outline-warning mt-2 mb-2">Add to card</button>
+    );
+  };
+
+  const showStock = (quantity) => {
+    return quantity > 0 ? (
+      <span className="badge badge-primary badge-pill">In Stock </span>
+    ) : (
+      <span className="badge badge-primary badge-pill">Out of Stock </span>
+    );
+  };
+
   return (
     <div className="card">
-      <div className="card-header card-header-1 ">{product.name}</div>
+      <div className="card-header name">{product.name}</div>
       <div className="card-body">
         <ShowImage item={product} url="product" />
         <p className="card-p  mt-2">{product.description.substring(0, 100)} </p>
-        <p className="card-p black-10">$ {product.price}</p>
+        <p className="card-p">$ {product.price}</p>
+        <p>Category: {product.category && product.category.name}</p>
+        <p>Added on {moment(product.createdAt).fromNow()}</p>
+
+        {showStock(product.quantity)}
+        <br />
+
         {showViewButton(showViewProductButton)}
-        <button className="btn btn-outline-warning mt-2 mb-2">
-          Add to card
-        </button>
+        {showAddToCartButton()}
       </div>
     </div>
   );
--- server/controllers/product.js
+++ server/controllers/product.js
@@ -4,15 +4,17 @@ const fs = require("fs");
 const Product = require("../models/product");
 
 exports.productById = (req, res, next, id) => {
-  Product.findById(id).exec((err, product) => {
-    if (err || !product) {
-      return res.status(400).json({
-        error: "Product not found",
-      });
-    }
-    req.product = product;
-    next();
-  });
+  Product.findById(id)
+    .populate("category")
+    .exec((err, product) => {
+      if (err || !product) {
+        return res.status(400).json({
+          error: "Product not found",
+        });
+      }
+      req.product = product;
+      next();
+    });
 };
 
 exports.read = (req, res) => {
