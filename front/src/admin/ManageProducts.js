9b9057b3a99c4413651e441ffb302c3b135d41fb
zizpzn <zizpzn@gmail.com>
2022-07-22 16:15:08 +0700

    get all products for admin CRUD

--- front/src/admin/ManageProducts.js
+++ front/src/admin/ManageProducts.js
@@ -2,9 +2,41 @@ import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import {
+  getProducts,
+  deleteProduct,
+  // getProduct,
+  // updateProduct,
+} from "./apiAdmin";
 
 const ManageProducts = () => {
-  //
+  const [products, setProducts] = useState([]);
+
+  const { user, token } = isAuthenticated();
+
+  const loadProducts = () => {
+    getProducts().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  const destroy = (productId) => {
+    deleteProduct(productId, user._id, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        loadProducts();
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
 
   return (
     <Layout
@@ -13,7 +45,32 @@ const ManageProducts = () => {
       className="container-fluid"
     >
       <div className="row">
-        <div>...</div>
+        <div className="col-12">
+          <h2 className="text-center">Total {products.length} products</h2>
+          <hr />
+          <ul className="list-group">
+            {products.map((product, index) => (
+              <li
+                key={index}
+                className="list-group-item d-flex justify-content-between align-items-center"
+              >
+                <strong>{product.name}</strong>
+
+                <Link to={`/admin/product/update/${product._id}`}>
+                  <span className="badge badge-warning badge-pill">Update</span>
+                </Link>
+
+                <span
+                  onClick={() => destroy(product._id)}
+                  className="badge badge-danger badge-pill"
+                >
+                  Delete
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        ...
       </div>
     </Layout>
   );
