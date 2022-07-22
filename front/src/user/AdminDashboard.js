51869cc4dad5310289c73d5936918b5ba533b8ec
zizpzn <zizpzn@gmail.com>
2022-07-22 09:30:23 +0700

    fetch all orders

--- front/src/user/AdminDashboard.js
+++ front/src/user/AdminDashboard.js
@@ -18,9 +18,16 @@ const AdminDashboard = () => {
               Create Category
             </Link>
           </li>
+
           <li className="list-group-item">
             <Link className="nav-link" to="/create/product">
               Create Product
+            </Link>
+          </li>
+
+          <li className="list-group-item">
+            <Link className="nav-link" to="/admin/orders">
+              View Orders
             </Link>
           </li>
         </ul>
