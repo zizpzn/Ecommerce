6cd1c26c3a1dbc82cf52dfe228590f824bdd41ff
zizpzn <zizpzn@gmail.com>
2022-07-22 15:09:12 +0700

    manage products for admin

--- front/src/user/AdminDashboard.js
+++ front/src/user/AdminDashboard.js
@@ -30,6 +30,12 @@ const AdminDashboard = () => {
               View Orders
             </Link>
           </li>
+
+          <li className="list-group-item">
+            <Link className="nav-link" to="/admin/products">
+              Manage Products
+            </Link>
+          </li>
         </ul>
       </div>
     );
